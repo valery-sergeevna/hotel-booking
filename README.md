c370ebecae9773232a56c599ac29ca26595cc671
Valery <lekalera777@gmail.com>
2020-08-27 22:53:48 +0300

    added burger-menu, tabs, sliders

---
+++ README.md
@@ -0,0 +1,4 @@
+# hotel-booking
+Hotel Booking Details Page цwas laid out with html/scss/js/jquery. It is responsive project on the web. Mobile and tablet friendly. Website template for Figma is available at https://dribbble.com/shots/11135604-Best-Tour-Plan-Hotel-Booking-Page-Figma-Freebie
+View website at https://valery-sergeevna.github.io/hotel-booking/
+
---
+++ js/script.js
@@ -0,0 +1,123 @@
+//jQuery
+$(document).ready(function () {
+    $('.slick-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+
+    $('.attractions').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+
+    });
+
+    $('.package__link').slick({
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: 'unslick'
+            },
+            {
+                breakpoint: 650,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+});
+
+//native JS
+
+//burger-menu
+window.addEventListener("DOMContentLoaded", () => {
+
+    //burger-menu
+    const burger = document.querySelector('.nav-toggle'),
+        burgerMenu = document.querySelector('.burger-menu'),
+        burgerClose = document.querySelector('.burger-menu__close');
+
+    burger.addEventListener('click', () => {
+        burgerMenu.classList.add('open');
+        burgerMenu.classList.add('animation');
+    });
+
+    burgerClose.addEventListener('click', () => {
+        burgerMenu.classList.remove('open');
+    });
+
+    //tabs
+    const packageLink = document.querySelector('.package__link'),
+        tabsContent = document.querySelectorAll('.content'),
+        tabs = document.querySelectorAll('.package__item');
+
+    const hideTabs = () => {
+        tabs.forEach(tab => {
+            tab.classList.remove('open', 'tabs_animation');
+            tab.classList.remove('package__item_active');
+        });
+
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('open');
+        });
+    };
+
+    const showTabs = (i = 4) => {
+        tabs[i].classList.add('package__item_active');
+        tabsContent[i].classList.add('open', 'tabs_animation');
+        tabsContent[i].classList.remove('hide');
+    };
+
+    hideTabs();
+    showTabs();
+
+    packageLink.addEventListener('click', e => {
+        const target = e.target;
+        if (target.classList.contains('package__item')) {
+            tabs.forEach((item, i) => {
+                //активировать функции 
+                if (target == item) {
+                    hideTabs();
+                    showTabs(i);
+                }
+
+            });
+        }
+    });
+
+});
